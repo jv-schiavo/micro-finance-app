698449414b9b5345f032ab2609ba8bd871aa618e
joao victor schiavo <jvgs.schiavo@gmail.com>
2026-01-08 21:45:32 +0000

    Add application service to CRUD operations

--- services/application_service.py
+++ services/application_service.py
@@ -11,3 +11,31 @@ class ApplicationService:
         JOIN product p ON a.product_id = p.product_id;
         """
         return self.db.fetchall(query)
+    
+    def create_application(self, customer_id, application_id, applicationDate, income, jobPosition, creditScore, amountRequested, loanPurpose,
+                           status, statusUpdateTime, officerNotes, loantermRequested):
+        ## Application MUST be created with only existing customers AND products
+        query = """
+        INSERT INTO application (customer_id, application_id, applicationDate, income, jobPosition, creditScore, amountRequested, loanPurpose,
+                           status, statusUpdateTime, officerNotes, loantermRequested)
+        VALUES (?,?, DATE('now'), ?,?,?,?,?,'Pending', DATE('now'), ?, ?)
+        """
+        self.db.execute(query, (customer_id, application_id, applicationDate, income, jobPosition, creditScore, amountRequested, loanPurpose,
+                           status, statusUpdateTime, officerNotes, loantermRequested))
+        
+    def update_application(self, application_id, status, statusUpdateTime):
+        query = """
+        UPDATE application
+        SET status = ?, statusUpdateTime = DATE('now')
+        WHERE application_id = ?
+        """
+
+        self.db.execute(query, (status, statusUpdateTime, application_id))
+
+    def delete_application(self, application_id):
+        query = """
+        DELETE FROM application
+        WHERE application_id = ?
+        """
+
+        self.db.execute(query, application_id)
