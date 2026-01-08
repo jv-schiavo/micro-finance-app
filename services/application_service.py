e1fdcf295405316a299eb2b6be8623522c218b61
joao victor schiavo <jvgs.schiavo@gmail.com>
2026-01-08 21:44:01 +0000

    Add application service to CRUD operations

--- services/application_service.py
+++ services/application_service.py
@@ -1,3 +1,13 @@
 class ApplicationService:
     def __init__(self, db):
         self.db = db
+
+    def get_all_applications(self):
+        query = """
+        SELECT a.application_id, c.name, p.product_name, a.applicationDate, a.income, a.jobPosition, a.creditScore,
+        a.amountRequested, a.loanPurpose, a.status, a.officerNotes, a.loanTermRequested
+        FROM application a
+        JOIN customer c ON a.customer_id = c.customer_id
+        JOIN product p ON a.product_id = p.product_id;
+        """
+        return self.db.fetchall(query)
